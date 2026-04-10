51a7ccceb57e1a8768f125cfe8edaa76c08746cd
anajulia0911 <anajblande04@gmail.com>
2026-04-10 23:00:56 +0000

    Criando o projeto

---
+++ ps2-prj1a/src/main/java/br/mackenzie/ps2_prj1a/Ps2Prj1aApplication.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ps2_prj1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ps2Prj1aApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ps2Prj1aApplication.class, args);
+	}
+
+}
---
+++ ps2-prj1a/src/test/java/br/mackenzie/ps2_prj1a/Ps2Prj1aApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mackenzie.ps2_prj1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ps2Prj1aApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
